a140f33add2356422f95c43893884cca45e50670
alvesoli <carolina.alvesoli@hotmail.com>
2022-12-19 20:35:37 -0300

    Exemplos operadores Java

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,6 +1,7 @@
 public class Operadores {
-    
-public static void public static void main(String[] args) {
-    
-}  
+    public static void main(String[] args) throws Exception {
+        String nomeCompleto = "LINGUAGEM" + "JAVA";
+
+        System.out.println(nomeCompleto);
+    }  
 }
