9506d0c6e92607449f7e56b8c5f7547968780bab
alvesoli <carolina.alvesoli@hotmail.com>
2022-12-20 08:28:42 -0300

    Unário Boolean False e True

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -2,10 +2,8 @@ public class Operadores {
     public static void main(String[] args) throws Exception {
         boolean variavel = true;
 
-        variavel = !variavel;
+        System.out.println(!variavel);
 
-    
         System.out.println(variavel);
         }
 }
-
