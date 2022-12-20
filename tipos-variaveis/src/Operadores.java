f78378227d3cedff2ddb1678c80e4cc7cec08661
alvesoli <carolina.alvesoli@hotmail.com>
2022-12-20 09:58:56 -0300

    Relacionais Verdadeiro ou False

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,12 +1,11 @@
 public class Operadores {
     public static void main(String[] args) throws Exception {
-            int a, b;
-            a = 6;
-            b = 6;
-            
-            String resultado = a==b ?"verdadeiro" : "false";
-           
-            System.out.println (resultado);
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        System.out.println("numeroUm é igual a numeroDois? " + simNao);
             
                 }
         }
