854de0f2d083177a36f0a8edf925b62dd67f8a26
alvesoli <carolina.alvesoli@hotmail.com>
2022-12-20 10:27:22 -0300

    Relacionais

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,13 +1,35 @@
+import javax.sound.midi.SysexMessage;
+
 public class Operadores {
     public static void main(String[] args) throws Exception {
+       String nomeUm = "Carolina";
+       String nomeDois = "Carolina";
+
+       System.out.println(nomeUm == nomeDois);
+       
+       
+       
         int numero1 = 1;
         int numero2 = 2;
 
         boolean simNao = numero1 == numero2;
 
+        if(numero1 < numero2){
+        System.out.print("A nossa condição é verdadeira");
+        }
+	   
+        
         System.out.println("numeroUm é igual a numeroDois? " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("numeroUm é diferente de  numeroDois? " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("numeroUm é maior que numeroDois? " + simNao);
             
-                }
+            }
         }
         
         
