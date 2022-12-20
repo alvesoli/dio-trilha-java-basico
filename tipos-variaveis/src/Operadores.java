302ebde9b77c27551bbbf6d3a82cc5cca240ff17
alvesoli <carolina.alvesoli@hotmail.com>
2022-12-20 08:02:02 -0300

    Unário 3

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,16 +1,11 @@
 public class Operadores {
     public static void main(String[] args) throws Exception {
-        
         int numero = 5;
 
-        numero = - numero;
-
+        numero++;
+   
         System.out.println(numero);
 
-        numero = numero * -1;
-
-        System.out.println(numero);
-        
         }
 }
 
