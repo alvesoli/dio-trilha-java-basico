2944ecb3a43e9913d2c60273ce92532d3148df66
alvesoli <carolina.alvesoli@hotmail.com>
2022-12-20 08:43:30 -0300

    Unário Boolean True

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -2,9 +2,7 @@ public class Operadores {
     public static void main(String[] args) throws Exception {
         boolean variavel = true;
 
-        System.out.println(!variavel);
-
         System.out.println(variavel);
-        
-    }
+        }
 }
+
